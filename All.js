70716cf1a8961fe29de724f169483e7b5f9aa28d
Jimmy L <jjlp98@gmail.com>
2020-05-29 19:14:28 +0000

    Not the push I expected but the push I needed

--- All.js
+++ All.js
@@ -12,48 +12,20 @@ function Projects() {
     window.location.href = 'projects.html';
 }
 
-function Tutorial() {
-    window.location.href = 'tutorial.html';
+function About() {
+    window.location.href = 'about.html';
 }
 
-function Contact() {
-    window.location.href = 'contact.html';
-}
-//Gallery slides
-loadBanner();
-var index = 0;
-
-function changeBanner() {
-    [].forEach.call(document.getElementsByClassName("galleryImages"), function(v, i) {
-        document.getElementsByClassName("galleryImages")[i].hidden = i !== index
-    });
-    index = (index + 1) % document.getElementsByClassName("galleryImages").length;
-    console.log("test");
-}
-
-function loadBanner() {
-    var srcs = ["Images/P006-Marty2015.gif", "Images/P090-WillyWonka.gif", "Images/P028-SorcererMickey.gif", "Images/P011-IronMan.gif", "Images/P070-WallE.gif"];
-    var c = 0;
-    imgs = [];
-    for(i = srcs.length - 1; i >= 0; i--) {
-        imgs[i] = new Image();
-        imgs[i].src = srcs[i];
-        imgs[i].onload = function() {
-            loaded();
-        };
+/*function togglePix(a) {
+    var e = document.getElementById(a);
+    if(!e) return true;
+    if(e.style.display == "none") {
+        e.style.display = "block";
+    } else {
+        e.style.display = "none";
     }
-    loaded = function() {
-        c++;
-        if(c === srcs.length) {
-            window.onload = function() {
-                setInterval(changeBanner, 2000)
-            };
-            changeBanner();
-        }
-    };
-    this.update = function() {};
-}
-
+    return false;
+}*/
 function toggleMe(a) {
     var e = document.getElementById(a);
     if(!e) return true;
@@ -66,6 +38,14 @@ function toggleMe(a) {
 }
 //Enlarge Image
 
+/*function show(smSrc, lgSrc) {
+    document.getElementById('largeImg').src = smSrc;
+    showLargeImagePanel();
+    unselectAll();
+    setTimeout(function() {
+        document.getElementById('largeImg').src = lgSrc;
+    }, 1);
+}*/
 function showImage(smSrc, lgSrc) {
     document.getElementById('largeImg').src = smSrc;
     showLargeImagePanel();
@@ -78,14 +58,67 @@ function showImage(smSrc, lgSrc) {
 function showLargeImagePanel() {
     document.getElementById('largeImgDiv').style.display = 'block';
 }
+
 //if keys[27]
 var keys = [];
 onekeydown = onkeyup = function(e) {
     e = e || event;
     keys[e.keycode] = e.type == 'keycode';
 };
+// Open the Modal
 
-function unselectAll() {
-    if(document.selection) document.selection.empty();
-    if(window.getSelection) window.getSelection().removeAllRanges();
+function openM() {
+    document.getElementById("myModal").style.display = "block";
 }
+// Close the Modal
+
+function closeM() {
+    document.getElementById("myModal").style.display = "none";
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function curr(n) {
+    showSlides(slideIndex = n);
+}
+
+/*var pixCt = document.querySelectorAll('#proud .squPix').length;
+for (var i=1; i<=pixCt; i++){
+    document.getElementById('output').innerHTML = i;
+}
+document.getElementById('output').innerHTML = pixCt;*/
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("galPic");
+    var captionText = document.getElementById("alt");
+    if(n > slides.length) {
+        slideIndex = 1
+    }
+    if(n < 1) {
+        slideIndex = slides.length
+    }
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    captionText.innerHTML = slides[slideIndex - 1].alt;
+}
+
+//arrow based gallery move
+document.addEventListener('keyup', function(e) {
+    if(e.defaultPrevented) {
+        return;
+    }
+    var key = e.key || e.keyCode;
+    if(key === 'ArrowLeft') {
+        plusSlides(-1);
+    } else if(key === 'ArrowRight') {
+        plusSlides(1);
+    }
+});
